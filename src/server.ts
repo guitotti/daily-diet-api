b30369ee5aaca7939b55051ada9e6831d7e7b979
guilherme_totti <dev.guilhermetotti@gmail.com>
2023-06-06 17:15:44 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from 'fastify'
+import { knex } from './database'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+  return tables
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running')
+  })
