47a6f7c93ad52d17e48ccc75f50c9db4cfffbc98
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-01-24 09:26:59 -0300

    ✨ feat: creation of middleware to check if session id exists

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,14 @@
+import 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    users: {
+      id: string
+      session_id: string
+      name: string
+      email: string
+      created_at: string
+      updated_at: string
+    }
+  }
+}
