b1805237747441b8ec0e58662a97754dbb430db0
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-01-24 09:35:22 -0300

    🐛 fix: addition of password on user types knex and fastify interfaces

--- src/@types/fastify.d.ts
+++ src/@types/fastify.d.ts
@@ -7,6 +7,7 @@ declare module 'fastify' {
       session_id: string
       name: string
       email: string
+      password: string
       created_at: string
       updated_at: string
     }
--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -7,6 +7,7 @@ declare module 'knex/types/tables' {
       session_id: string
       name: string
       email: string
+      password: string
       created_at: string
       updated_at: string
     }
